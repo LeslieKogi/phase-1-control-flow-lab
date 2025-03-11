490322df0db3a401206a0d3984acec04dccc1d21
Leslie Kogi <lesliekogi@gmail.com>
2025-03-11 11:32:30 +0300

    completes functions

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if(someValue<=400){
+    result=`This one is on me!`
+  }
+  else if(someValue<2000){
+    result=`That will be twenty bucks.`
+  }
+  else if (someValue>2000 && someValue <=2500){
+    result= `I will gladly take your thirty bucks.`
+  }
+  else{
+    result=`No can do.`
+  }
+  return result
+}
+console.log(scuberGreetingForFeet(230))
+console.log (scuberGreetingForFeet(9300))
+
+
+function ternaryCheckCity(city){
+return city==="NYC"? "Ok, sounds good.": "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+console.log(ternaryCheckCity(null))
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case "generous":
+      return `Thank you so much.`;
+  
+      case "not as generous":
+        return`Thank you.`;
+      
+        default:
+          return `Bye.`;
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+console.log(switchOnCharmFromTip("generous"))
